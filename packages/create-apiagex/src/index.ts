f8920ced9abd6f5bf222dc83918211a85d5c25ae
aditya wsl <bestemplin@gmail.com>
2026-05-18 02:48:35 +0000

    Release 0.8.12

--- packages/create-apiagex/src/index.ts
+++ packages/create-apiagex/src/index.ts
@@ -9,7 +9,7 @@ import { resolveAnswers } from "./prompts.js";
 import { createScaffoldFiles, renderPlan } from "./scaffold.js";
 import type { CliResult, RunCliOptions } from "./create-apiagex.type.js";
 
-const packageVersion = "0.8.11";
+const packageVersion = "0.8.12";
 
 export async function runCli(args: string[], cwd = process.cwd(), io: RunCliOptions = {}): Promise<CliResult> {
   const parsed = parseArgs(args);
